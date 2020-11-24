e1ccc5df70e449d207cc65035809209dcb0e39f6
fabian6 <fabian6_03@hotmail.com>
2020-11-23 23:53:40 -0700

    Primer commit

---
+++ database.php
@@ -0,0 +1,12 @@
+<?php
+    $connection = mysqli_connect(
+        'localhost',
+        'root',
+        '',
+        'task-db'
+    );
+
+    // if($connection){
+    //     echo "Database is connected";
+    // }
+?>
